24f48798f457ba20ff448a3ca47a88162dc01dc2
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-05-05 14:03:39 +0100

    Create Variants.tsx

---
+++ src/Variants.tsx
@@ -0,0 +1,16 @@
+import { variants as SSVariant } from "styled-system";
+
+interface Props {
+  prop?: string;
+  scale?: string;
+  variants?: object;
+}
+
+export const variant = ({ prop = "variant", scale, variants = { primary: {} } }: Props) =>
+  SsVariant({ prop, scale: scale ? `variants.${scale}` : undefined, variants });
+
+export const IntentVariants = variant({ scale: "intents" });
+
+export const TypographyVariants = variant({ scale: "typography" });
+
+export default variant;
