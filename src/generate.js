de0ba4f0d3b20f5acd64360d56ba14f54acc73ce
The-Code-Monkey <andy.wilson1234@gmail.com>
2021-05-20 08:24:00 +0000

    fix: fix linting

--- src/generate.js
+++ src/generate.js
@@ -1,27 +1,28 @@
-const StyleDictionary = require("style-dictionary");
-const fs = require("fs-extra");
+const StyleDictionary = require('style-dictionary');
+const fs = require('fs-extra');
 const path = require('path');
-const _ = require("lodash");
-const Color = require("tinycolor2");
+const _ = require('lodash');
+const Color = require('tinycolor2');
 
-const Config = require("./config.json");
+const Config = require('./config.json');
 
-const configFileNames = ["orchard.theme.config.json"];
+const configFileNames = ['orchard.theme.config.json'];
 
-const resolveConfig = () => new Promise(resolve => {
-  for (let i = 0; i < configFileNames.length; i++) {
-    fs.exists(`${process.cwd()}/${configFileNames[i]}`, (exists) => {
-      if (exists) {
-        console.log(`${process.cwd()}/${configFileNames[i]} -- Exists`);
-        resolve(`${process.cwd()}/${configFileNames[i]}`);
-      }
-    });
-  }
-});
+const resolveConfig = () =>
+  new Promise(resolve => {
+    for (let i = 0; i < configFileNames.length; i++) {
+      fs.exists(`${process.cwd()}/${configFileNames[i]}`, exists => {
+        if (exists) {
+          console.log(`${process.cwd()}/${configFileNames[i]} -- Exists`);
+          resolve(`${process.cwd()}/${configFileNames[i]}`);
+        }
+      });
+    }
+  });
 
 const minifyDictionary = obj => {
   const toRet = {};
-  if (Object.keys(obj).includes("value")) {
+  if (Object.keys(obj).includes('value')) {
     return obj.value;
   }
   // eslint-disable-next-line no-restricted-syntax
@@ -35,7 +36,7 @@ const minifyDictionary = obj => {
 };
 
 const nestedJson = dictionary => {
-  if (dictionary.allProperties[0].name === "neutral_base") {
+  if (dictionary.allProperties[0].name === 'neutral_base') {
     const properties = dictionary.properties;
 
     properties.color.neutrals = properties.color.neutral_base;
@@ -74,7 +75,7 @@ export default tokens;
 
     return `const tokens = ${JSON.stringify(
       {
-        breakpoints: Object.keys(minified).map(key => minified[key])
+        breakpoints: Object.keys(minified).map(key => minified[key]),
       },
       null,
       2
@@ -91,27 +92,33 @@ export default tokens;
 };
 
 StyleDictionary.registerFormat({
-  name: "custom/nested/json",
-  formatter: nestedJson
+  name: 'custom/nested/json',
+  formatter: nestedJson,
 });
 
 StyleDictionary.registerFormat({
-  name: "custom/intent_tokens",
-  formatter: _.template(fs.readFileSync(path.resolve(__dirname, "./templates/intent_tokens.template")))
+  name: 'custom/intent_tokens',
+  formatter: _.template(
+    fs.readFileSync(
+      path.resolve(__dirname, './templates/intent_tokens.template')
+    )
+  ),
 });
 
 StyleDictionary.registerFormat({
-  name: "custom/neutrals_tokens",
-  formatter: _.template(fs.readFileSync(path.resolve(__dirname, "./templates/neutrals_tokens.template")))
+  name: 'custom/neutrals_tokens',
+  formatter: _.template(
+    fs.readFileSync(
+      path.resolve(__dirname, './templates/neutrals_tokens.template')
+    )
+  ),
 });
 
 StyleDictionary.registerTransform({
-  name: "color/makeShades",
-  type: "value",
+  name: 'color/makeShades',
+  type: 'value',
   matcher(prop) {
-    return (
-      prop.attributes.type === "intents"
-    );
+    return prop.attributes.type === 'intents';
   },
   transformer(prop) {
     const color = Color(prop.value);
@@ -119,8 +126,8 @@ StyleDictionary.registerTransform({
     const paletteLight = [];
     const paletteDark = [];
 
-    const colorBase = Color("#222222");
-    const colorLight = Color("#FFF");
+    const colorBase = Color('#222222');
+    const colorLight = Color('#FFF');
 
     paletteLight.push(color.toHexString());
     paletteLight.push(Color.mix(color, colorBase, 10).toHexString());
@@ -138,19 +145,19 @@ StyleDictionary.registerTransform({
     subtheme.dark = paletteDark;
 
     return subtheme;
-  }
+  },
 });
 
 StyleDictionary.registerTransform({
-  name: "color/makeNeutrals",
-  type: "value",
+  name: 'color/makeNeutrals',
+  type: 'value',
   matcher(prop) {
     // this is an example of a possible filter (based on the "cti" values) to show how a "matcher" works
-    return prop.attributes.type === "neutral_base";
+    return prop.attributes.type === 'neutral_base';
   },
   transformer(prop) {
     const colorBase = Color(prop.value);
-    const colorLight = Color("#FFFFFF");
+    const colorLight = Color('#FFFFFF');
     const neutrals = {};
     const paletteLight = [];
     const paletteDark = [];
@@ -175,7 +182,7 @@ StyleDictionary.registerTransform({
       70,
       80,
       90,
-      100
+      100,
     ];
 
     percentages.forEach(mixPercentage => {
@@ -191,36 +198,44 @@ StyleDictionary.registerTransform({
     neutrals.dark = paletteDark;
 
     return neutrals;
-  }
+  },
 });
 
 StyleDictionary.registerAction({
-  name: "copy_assets",
+  name: 'copy_assets',
   do: function(dictionary, config) {
-    console.log("Copying assets directory");
+    console.log('Copying assets directory');
     fs.copySync(config.buildPath, process.cwd() + `theme/${config.buildPath}`);
   },
   undo: function(dictionary, config) {
-    console.log("Cleaning assets directory");
-    fs.removeSync(config.buildPath + "dist");
-  }
+    console.log('Cleaning assets directory');
+    fs.removeSync(config.buildPath + 'dist');
+  },
 });
 
-const generate = async (brand = "default") => {
+const generate = async (brand = 'default') => {
   const userConfigFile = await resolveConfig();
   const userConfig = fs.readJsonSync(userConfigFile);
-  const outputDir = userConfig.outputDir ? `${process.cwd()}${userConfig.outputDir}theme/dist` : `${process.cwd()}/theme/dist`;
-  const customSrcDir = userConfig.srcDir ? `${process.cwd()}${userConfig.srcDir}theme/src` : `${process.cwd()}/theme/src`;
-  
+  const outputDir = userConfig.outputDir
+    ? `${process.cwd()}${userConfig.outputDir}theme/dist`
+    : `${process.cwd()}/theme/dist`;
+  const customSrcDir = userConfig.srcDir
+    ? `${process.cwd()}${userConfig.srcDir}theme/src`
+    : `${process.cwd()}/theme/src`;
+
   fs.ensureDir(outputDir);
   const ConfigWithSource = Config;
   if (fs.existsSync(customSrcDir)) {
-    console.log("Using your config");
+    console.log('Using your config');
     ConfigWithSource.source = [`${customSrcDir}/**/*.json`];
   } else {
-    console.log("Using default config");
-    console.log(path.resolve(__dirname, `theme/${brand.toLowerCase()}/**/*.json`));
-    ConfigWithSource.source = [path.resolve(__dirname, `theme/${brand.toLowerCase()}/**/*.json`)];
+    console.log('Using default config');
+    console.log(
+      path.resolve(__dirname, `theme/${brand.toLowerCase()}/**/*.json`)
+    );
+    ConfigWithSource.source = [
+      path.resolve(__dirname, `theme/${brand.toLowerCase()}/**/*.json`),
+    ];
   }
 
   const BaseStyleDictionary = StyleDictionary.extend(ConfigWithSource);
@@ -228,22 +243,22 @@ const generate = async (brand = "default") => {
   BaseStyleDictionary.buildAllPlatforms();
 
   fs.copySync(`./dist`, outputDir);
-  
+
   fs.removeSync(`./dist`);
 };
 
 const argv = process.argv.slice(2);
 
-if (argv[0] === "--brand") {
+if (argv[0] === '--brand') {
   if (argv[1]) {
     // eslint-disable-next-line no-console
     console.log(`\nBuilding ${argv[1]} tokens...`);
     generate(argv[1]);
   } else {
-    throw new Error("Must specify brand when using --brand argument");
+    throw new Error('Must specify brand when using --brand argument');
   }
 } else {
   // eslint-disable-next-line no-console
-  console.log("\nBuilding Default tokens...");
+  console.log('\nBuilding Default tokens...');
   generate();
 }
